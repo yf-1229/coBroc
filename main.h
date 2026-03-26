4fc510fbd8264e0818ee3de59c4574671882737c
yuta <yf1229@proton.me>
2026-03-26 23:55:15 +0900

    sample code

--- main.h
+++ main.h
@@ -1,18 +1,15 @@
-//
-// Created by yuta on 2026/03/26.
-//
-
 #pragma once
 
 #ifndef CODES_MAIN_H
 #define CODES_MAIN_H
 
-#include <iostream>
-#include <random>
 #include "pico/stdlib.h"
-#include "pico/multicore.h"
-#include "pico/mutex.h"
-
+#include <cstdint>
+#include <cstdio>
+#include <array>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
 
 #define keyA 15
 #define keyB 17
@@ -24,7 +21,7 @@
 #define keyRight 20
 #define keyCtrl 3
 
-#endif //CODES_MAIN_H
-
-
 constexpr uint16_t LCD_REFRESH_DELAY_MS = 50;
+int LCD();
+
+#endif // CODES_MAIN_H
